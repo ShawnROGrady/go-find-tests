76903ea21b6b1a4bb5847d7cc1474ac4a7df35e2
ShawnROGrady <shawn.r.ogrady@gmail.com>
2019-10-22 21:39:39 -0500

    [cmd] added help flags and usage message

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 
@@ -16,8 +17,24 @@ func main() {
 		line, col       int
 		err             error
 		includeSubtests = flag.Bool("include-subs", false, "Find specific sub-tests which cover the specified block")
+		helpShort       = flag.Bool("h", false, "Print a help message and exit")
+		help            = flag.Bool("help", false, "Print a help message and exit")
 	)
 	flag.Parse()
+	if *help || *helpShort {
+		fmt.Fprintf(os.Stdout, "Usage: %s [-include-subs] filepath line col\n", os.Args[0])
+		fmt.Fprintf(os.Stdout, "Description: %s prints the tests (and optionally sub tests) which cover a specified block of code\n", os.Args[0])
+		fmt.Fprint(os.Stdout, "Required arguments:\n")
+		fmt.Fprint(os.Stdout, "\tfilepath: path to the file to check\n")
+		fmt.Fprint(os.Stdout, "\tline: the line of the block to check\n")
+		fmt.Fprint(os.Stdout, "Optional arguments:\n")
+		fmt.Fprint(os.Stdout, "\trow: the row of the block to check\n")
+		fmt.Fprint(os.Stdout, "Optional flags:\n")
+		flag.VisitAll(func(f *flag.Flag) {
+			fmt.Fprintf(os.Stdout, "\t-%s: %s [default = %v]\n", f.Name, f.Usage, f.DefValue)
+		})
+		os.Exit(0)
+	}
 	args := flag.Args()
 	if len(args) < 2 {
 		log.Fatal("Path and line are required")
@@ -46,5 +63,5 @@ func main() {
 			log.Fatalf("Error determining covering tests: %#v", err)
 		}
 	}
-	fmt.Printf("%s\n", coveredBy)
+	fmt.Fprintf(os.Stdout, "%s\n", coveredBy)
 }
